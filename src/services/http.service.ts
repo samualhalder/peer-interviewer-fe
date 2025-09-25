d3a35b9da1bce649e2626e94dfc1fa2f820c4c74
samualhalder <samual.halder2020@gmail.com>
2025-09-25 22:48:22 +0530

    change in public base url be

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import TokenUtils from "../utils/token.utils";
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string;
-axios.defaults.baseURL = baseURL;
+axios.defaults.baseURL = baseURL + "/api/";
 const token = TokenUtils?.getToken() as string;
 const setJWT = (tk: string) => {
   axios.defaults.headers.common["Authorization"] = `Bearer ${tk}`;
